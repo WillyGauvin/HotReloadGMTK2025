1518bfc12494deb531d03ef7863fe07f411c3c6b
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2025-07-31 14:37:58 +0300

    Make thow controls more granular

--- Assets/Scripts/Controls/BlockGrabber.cs
+++ Assets/Scripts/Controls/BlockGrabber.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 
 public class BlockGrabber : MonoBehaviour
 {
+    [SerializeField] private Camera cam;
     [SerializeField] private Transform positionCastCenter;
     [SerializeField] private Transform positionWhenHeld;
 
@@ -11,40 +12,50 @@ public class BlockGrabber : MonoBehaviour
     [SerializeField] private InputActionReference grabInputAction;
 
     [Header("Parameters")]
-    [SerializeField] private float radius;
+    [SerializeField] private float grabRadius;
+    [SerializeField] private bool useMouse;
     [SerializeField] private LayerMask layerMask;
     [SerializeField] private float throwMinHorizontal;
     [SerializeField] private float throwMaxHorizontal;
     [SerializeField] private float throwMinVertical;
     [SerializeField] private float throwMaxVertical;
-    [SerializeField] private Vector2 throwMultiplier;
+    [SerializeField, Range(0f, 1f)] private float throwInterruptionVelocity;
+    [SerializeField, Range(0f, 10f)] private float throwInterruptionInvalidationTime;
 
 
     private InstructionBlock grabbedBlock;
-
-    private void Start()
-    {
-        grabInputAction.action.performed += (context) =>
-        {
-            if (context.performed)
-            {
-                if (!grabbedBlock) Grab();
-                else Ungrab();
-            }
-        };
-    }
+    private InstructionBlock lastThrownBlock;
+    private float lastThrownTimeSec;
 
     private void Update()
     {
         if (grabbedBlock)
         {
             grabbedBlock.transform.position = positionWhenHeld.position;
+            if (grabInputAction.action.WasPressedThisFrame())
+            {
+                Ungrab();
+            }
+        }
+        else if (grabInputAction.action.WasPressedThisFrame())
+        {
+            Grab();
+        }
+        else if (grabInputAction.action.WasReleasedThisFrame() && (Time.time - lastThrownTimeSec) < throwInterruptionInvalidationTime)
+        {
+            var blockBody = lastThrownBlock.GetComponent<Rigidbody>();
+            blockBody.linearVelocity = new Vector3(
+                blockBody.linearVelocity.x * throwInterruptionVelocity,
+                blockBody.linearVelocity.y * (blockBody.linearVelocity.y < 0f ? 1f : throwInterruptionVelocity),
+                blockBody.linearVelocity.z * throwInterruptionVelocity
+            );
+            lastThrownBlock = null;
         }
     }
 
     public void Grab()
     {
-        var hits = Physics.OverlapSphere(positionCastCenter.position, radius, layerMask, QueryTriggerInteraction.Ignore);
+        var hits = Physics.OverlapSphere(positionCastCenter.position, grabRadius, layerMask, QueryTriggerInteraction.Ignore);
         InstructionBlock nearest = null;
         var nearestDist = float.PositiveInfinity;
         foreach (var item in hits)
@@ -67,6 +78,7 @@ public class BlockGrabber : MonoBehaviour
         block.Body.isKinematic = true;
 
         block.GetComponent<Collider>().enabled = false;
+        lastThrownBlock = null;
         grabbedBlock = block;
     }
 
@@ -74,26 +86,41 @@ public class BlockGrabber : MonoBehaviour
     {
         var resultV = new Vector3();
         var heldInput = movementInputAction.action.ReadValue<Vector2>();
-        if (heldInput.y < 0f)
+        if (useMouse)
         {
-            resultV = new Vector3(
-                throwMinHorizontal * heldInput.x * (1f + heldInput.y),
-                throwMinVertical * (1f + heldInput.y),
+            var screenPos = Mouse.current.position.ReadValue();
+            var worldPos = cam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, transform.position.z - cam.transform.position.z));
+            resultV = Vector3.Scale((transform.position - worldPos).normalized, new Vector3(
+                Mathf.Lerp(throwMinHorizontal, throwMaxHorizontal, 1f + heldInput.y),
+                Mathf.Lerp(throwMinVertical, throwMaxVertical, 1f + heldInput.y),
                 0f
-            );
+            ));
         }
         else
         {
-            resultV = new Vector3(
-                heldInput.x == 0f ? 0f : Mathf.Lerp(throwMinHorizontal, throwMaxHorizontal, Mathf.Abs(heldInput.x)) * Mathf.Sign(heldInput.x),
-                Mathf.Lerp(throwMinVertical, throwMaxVertical, heldInput.y),
-                0f
-            );
+            if (heldInput.y < 0f)
+            {
+                resultV = new Vector3(
+                    throwMinHorizontal * heldInput.x * (1f + heldInput.y),
+                    throwMinVertical * (1f + heldInput.y),
+                    0f
+                );
+            }
+            else
+            {
+                resultV = new Vector3(
+                    heldInput.x == 0f ? 0f : Mathf.Lerp(throwMinHorizontal, throwMaxHorizontal, Mathf.Abs(heldInput.x)) * Mathf.Sign(heldInput.x),
+                    Mathf.Lerp(throwMinVertical, throwMaxVertical, heldInput.y),
+                    0f
+                );
+            }
         }
         grabbedBlock.Body.isKinematic = false;
         grabbedBlock.Body.linearVelocity = resultV;
 
         grabbedBlock.GetComponent<Collider>().enabled = true;
+        lastThrownTimeSec = Time.time;
+        lastThrownBlock = grabbedBlock;
         grabbedBlock = null;
     }
 }
