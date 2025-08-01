5239f6a9d5c266a5bdfefc1c180d0eb596d64ba3
Seth Burchfield <cheesefrycook00@gmail.com>
2025-08-01 12:32:04 -0500

    command block highlighting

--- Assets/Scripts/Robot/CommandBlock.cs
+++ Assets/Scripts/Robot/CommandBlock.cs
@@ -1,3 +1,6 @@
+using DG.Tweening;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum InputType { Forward, Rotate_Clockwise, Rotate_CounterClockwise };
@@ -6,6 +9,10 @@ public class CommandBlock : MonoBehaviour, IInteractable
     public InputType inputType;
 
     public bool IsTeleporting = false;
+
+    public MeshRenderer mesh;
+    public float highlightIntensity = 0.5f;
+    public float highlightEaseTime = 0.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -28,11 +35,22 @@ public class CommandBlock : MonoBehaviour, IInteractable
     public void LookAt()
     {
         //GetComponent<Renderer>().material.color = Color.red;
+        Highlight(highlightIntensity);
     }
 
     public void LookAway()
     {
         //GetComponent<Renderer>().material.color = Color.blue;
+        Highlight(0.0f);
     }
 
+    private void Highlight(float amount)
+    {
+        List<Material> mats = new List<Material>();
+        mesh.GetMaterials(mats);
+        foreach (Material mat in mats)
+        {
+            Tween tween = mat.DOFloat(amount, "_HighlightAmount", highlightEaseTime);
+        }
+    }
 }
