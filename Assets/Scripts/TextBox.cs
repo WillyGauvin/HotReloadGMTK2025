0ac630720ef1dcd98e1cc325128d8c0437cf3957
Izaak Aiddi <izaakaidid613@gmail.com>
2025-08-02 12:24:25 -0400

    continued work on displaying text box

--- Assets/Scripts/TextBox.cs
+++ Assets/Scripts/TextBox.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class TextBox : MonoBehaviour
@@ -9,7 +11,9 @@ public class TextBox : MonoBehaviour
 
     [SerializeField] private string currentText;
     [SerializeField] private BoxCollider currentTrigger;
-    [SerializeField] private int currentIndex = 0;
+    [SerializeField] private int currentTextIndex = 0;
+    [SerializeField] private int currentTriggerIndex = 0;
+    [SerializeField] private int maxTextBoxDisplayTime = 0;
 
     public GameObject speaker;
     public List<BoxCollider> allTriggers;
@@ -44,24 +48,17 @@ public class TextBox : MonoBehaviour
         {
             if (other.TryGetComponent<Player>(out Player player) || other.TryGetComponent<RobotController>(out RobotController robot))
             {
-                currentIndex++;
 
-                //If There is a speaker, make it spawn from them into the middle
-                if (speaker != null)
-                {
+                DisplayNextTextBox();
 
-                }
-                //else just make the text box appear in the center of the screen
-                else
-                {
-
-                }
             }
         }
     }
     void Start()
     {
-
+        currentTriggerIndex = 0;
+        currentTextIndex = 0;
+        StartCoroutine(DisplayTextBox());
     }
 
     // Update is called once per frame
@@ -70,24 +67,47 @@ public class TextBox : MonoBehaviour
 
     }
 
-    void DisplayTextBox()
+    IEnumerator DisplayTextBox()
     {
         if (currentText != null)
         {
-            //do thing ;)
+            // If There is a speaker, make it spawn from them into the middle
+            if (speaker)
+            {
+
+                //do thing :)
+                yield return new WaitForSeconds(maxTextBoxDisplayTime);
+            }
+            else
+            {
+
+                //do thing :)
+                yield return new WaitForSeconds(maxTextBoxDisplayTime);
+            }
         }
     }
 
     void HideTextBox()
     {
-        //hide the text box
+        //hide the text box canvas
     }
 
     void DisplayNextTextBox()
     {
-        if (allTriggers.Count > 0 && allText.Count > 1)
+        if (allTriggers.Count > 0 && allText.Count > 0)
         {
-
+            if (currentTriggerIndex < allTriggers.Count - 1)
+            {
+                HideTextBox();
+            }
+            else
+            {
+                currentText = allText[currentTextIndex];
+                currentTrigger = allTriggers[currentTriggerIndex];
+            }
+            StartCoroutine(DisplayTextBox());
+            currentTriggerIndex++;
+            currentTextIndex++;
         }
     }
 }
